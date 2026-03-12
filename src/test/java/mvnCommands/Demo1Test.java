94edefb735102296b449150c535755875f010f82
Sudeshna Pathak <pathaksudeshna92@gmail.com>
2026-03-12 15:11:32 +0530

    Main Branch : 2nd Commit

--- src/test/java/mvnCommands/Demo1Test.java
+++ src/test/java/mvnCommands/Demo1Test.java
@@ -12,6 +12,6 @@ public class Demo1Test {
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.amazon.com/");
 		driver.quit();
-		System.out.println("Push from main branch");
+		System.out.println("2nd Push from main branch");
 	}
 }
