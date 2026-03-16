e8a266c885df0d4e9d57f2b6591009896ccb03ec
Sudeshna Pathak <pathaksudeshna92@gmail.com>
2026-03-16 18:14:18 +0530

    update test & pom.xml

--- src/test/java/mvnCommands/Demo4Test.java
+++ src/test/java/mvnCommands/Demo4Test.java
@@ -2,8 +2,10 @@ package mvnCommands;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
 public class Demo4Test {
+	@Test
 	public void sample4Test()
 	{
 		WebDriver driver = new ChromeDriver();
